a32eab549526195780c47a2fb124e0f9e0c9bea2
EC2 Default User <ec2-user@ip-172-31-29-215.us-east-2.compute.internal>
2018-07-12 05:51:38 +0000

    add grid column

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -1,25 +1,27 @@
 @extends ('layouts/app')
 @section('content')
     <h1>タスク一覧</h1>
-
+    
     @if(count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>状態</th>
-                    <th>タスク</th>
-                </tr>
-            </thead>
-            
-            @foreach ($tasks as $task)
-                <tr>
-                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                    <td>{{ $task->status }} </td>
-                    <td>{{ $task->content }} </td>
-                </tr>
-            @endforeach
-        </table>
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6"> 
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>状態</th>
+                        <th>タスク</th>
+                    </tr>
+                </thead>
+                
+                @foreach ($tasks as $task)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                        <td>{{ $task->status }} </td>
+                        <td>{{ $task->content }} </td>
+                    </tr>
+                @endforeach
+            </table>
+        </div>
     @endif
     {!! link_to_route('tasks.create', ' タスク更新登録', null, ['class' => 'btn btn-primary glyphicon glyphicon-plus']) !!}
     
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,22 +2,24 @@
 @section ('content')
     <h1>id = {{ $task->id }} タスクの詳細ページ</h1>
     
-    <table class="table table-bordered">
-        <tr>
-            <th>id</th>
-            <td> {{ $task->id }} </td>
-        </tr>
-        
-        <tr>
-            <th>状態</th>
-            <td> {{ $task->status }} </td>
-        </tr>
-        
-        <tr>
-            <th>タスク</th>
-            <td> {{ $task->content }} </td>
-        </tr>
-    </table>
+    <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6"> 
+        <table class="table table-bordered">
+            <tr>
+                <th>id</th>
+                <td> {{ $task->id }} </td>
+            </tr>
+            
+            <tr>
+                <th>状態</th>
+                <td> {{ $task->status }} </td>
+            </tr>
+            
+            <tr>
+                <th>タスク</th>
+                <td> {{ $task->content }} </td>
+            </tr>
+        </table>
+    </div>
     
     {!! link_to_route('tasks.edit', ' このタスク編集', ['id' => $task->id], ['class' => 'btn btn-default glyphicon glyphicon-pencil']) !!}
     
