8e5fd2524da1698ec88699c62aa2b3a353ceaae5
EC2 Default User <ec2-user@ip-172-31-29-215.us-east-2.compute.internal>
2018-07-21 16:04:35 +0000

    edit signup button

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,31 +2,32 @@
 @section('content')
     <h1>タスク一覧</h1>
     
-    @if(count($tasks) > 0)
         <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6"> 
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th>id</th>
-                        <th>状態</th>
-                        <th>タスク</th>
-                    </tr>
-                </thead>
-                
-                <tbody>
-                    @foreach ($tasks as $task)
+            @if(count($tasks) > 0)
+                <table class="table table-striped">
+                    <thead>
                         <tr>
-                            <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
-                            <td>{{ $task->status }} </td>
-                            <td>{{ $task->content }} </td>
+                            <th>id</th>
+                            <th>状態</th>
+                            <th>タスク</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                    </thead>
+                    
+                    <tbody>
+                        @foreach ($tasks as $task)
+                            <tr>
+                                <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                                <td>{{ $task->status }} </td>
+                                <td>{{ $task->content }} </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                
+            @endif
             
             {!! link_to_route('tasks.create', ' タスク更新登録', null, ['class' => 'btn btn-primary glyphicon glyphicon-plus']) !!}
-    
         </div>
-    @endif
+    
     
 @endsection
