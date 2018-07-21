5a00486ae6022f45ff1623dec764f0f63861b4c4
EC2 Default User <ec2-user@ip-172-31-29-215.us-east-2.compute.internal>
2018-07-21 02:01:51 +0000

    delete list user page

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -15,12 +15,18 @@ class UsersController extends Controller
     }
     
     public function show($id){
-        $user = User::find($id);
-        $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
         
-        $data = ['user' => $user, 'tasks' => $tasks];
-        $data += $this->counts($user);
+        return redirect("/");
+        //$user = User::find($id);
+        // if(\Auth::id() != $user->id){
+        //     return redirect('/');
+        // } else{
+        //     $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
         
-        return view('users.show', $data);
+        //     $data = ['user' => $user, 'tasks' => $tasks];
+        //     $data += $this->counts($user);
+            
+        //     return view('users.show', $data);
+        // }
     }
 }
